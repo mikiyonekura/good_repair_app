e52723a2809bf9cef853b339a0cd1021f68e5bc0
yonekuramiki <mikiyonekura4040@gmail.com>
2024-08-08 23:31:59 +0900

    開発中の文言を削除

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return(
         <footer className="footer text_contents">
-            このサイトは現在，開発中です．記載されている内容の参照及び，フォーム等の利用はご遠慮ください．ご承知の程お願いします．
+            {/* このサイトは現在，開発中です．記載されている内容の参照及び，フォーム等の利用はご遠慮ください．ご承知の程お願いします． */}
         </footer>
     )
 }
