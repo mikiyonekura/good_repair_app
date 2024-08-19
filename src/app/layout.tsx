50d41db1e79f04a8db61584be5f071364add151f
yonekuramiki <mikiyonekura4040@gmail.com>
2024-08-19 21:18:47 +0900

    SEO対策でキーワードを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ const inter = Inter({ subsets: ["latin"] });
 const m_plus = M_PLUS_Rounded_1c({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata: Metadata = {
-  title: "大分の住宅キズ補修ならグッドリペア大分",
-  description: "大分の補修工事をお探しの方はグッドリペアがおすすめ！",
+  title: "住宅の傷を補修するならグッドリペア大分 | Good Repair Oita | 大分市、別府市を中心に活動するリペア業者",
+  description: "生活しているうちに、フローリングやドア、家具などにキズが入ってしまった。そんな時にリペア技術を利用してみませんか？大分市、別府市は無料です。それ以外は、JR大分駅を起点としてガソリン代1km当たり50円と高速道路利用時は高速道路代を加算させていただきます。",
 };
 
 export default function RootLayout({
